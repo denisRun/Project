023d007fb1660dee47fe23eddd7c655705f2efd1
denisRun <polozov.denis77@gmail.com>
2021-07-11 10:24:42 +0300

    CategoryCotrollerTests GetById and GetAll

--- NixProjectV2/HotelAPI/Models/CategoryModel.cs
+++ NixProjectV2/HotelAPI/Models/CategoryModel.cs
@@ -11,5 +11,23 @@ namespace HotelAPI.Models
         public string Name { set; get; }
         public decimal Price { set; get; }
         public int Bed { set; get; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is CategoryModel)
+            {
+                var objCM = obj as CategoryModel;
+                return this.Id == objCM.Id &&
+                    this.Name == objCM.Name &&
+                    this.Price == objCM.Price &&
+                    this.Bed == objCM.Bed;
+            }
+            else
+            {
+                return base.Equals(obj);
+            }
+
+        }
+
     }
 }
